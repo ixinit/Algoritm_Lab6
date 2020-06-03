cc449c42cddd92ca11a9873491e55b35c1db20c9
ixinit <asrtemmashenkoff20158@gmail.com>
2020-06-04 01:31:47 +0300

    Create repo

---
+++ Lab6/Lab6/Lab6.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Lab6
+{
+    partial class Lab6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFirst = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.tbSecond = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Перва труба";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вторая труба";
+            // 
+            // tbFirst
+            // 
+            this.tbFirst.Location = new System.Drawing.Point(28, 53);
+            this.tbFirst.Name = "tbFirst";
+            this.tbFirst.Size = new System.Drawing.Size(100, 20);
+            this.tbFirst.TabIndex = 2;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(100, 134);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 3;
+            this.btnClick.Text = "Решить";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // tbSecond
+            // 
+            this.tbSecond.Location = new System.Drawing.Point(158, 52);
+            this.tbSecond.Name = "tbSecond";
+            this.tbSecond.Size = new System.Drawing.Size(100, 20);
+            this.tbSecond.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(62, 102);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(116, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Заполнят вместе за: ";
+            // 
+            // Lab6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbSecond);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.tbFirst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Lab6";
+            this.Text = "Lab6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFirst;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.TextBox tbSecond;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
---
+++ Lab6/Lab6/Lab6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Lab6 : Form
+    {
+        private static double first, second;
+        public Lab6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            double result;
+            if ((Double.TryParse(tbFirst.Text, out first)) && (Double.TryParse(tbSecond.Text, out second)))
+            {
+                result = 1 / (1 / first + 1 / second);
+                lblResult.Text = "Заполнят вместе за:" + result;
+            }
+            else
+                lblResult.Text = "Ошибка";
+        }
+    }
+}
